2973c1798c96cef7669c704336441ee1aaa88dbc
ibrahimkfksl <ibrahim.kafkasli@hotmail.com>
2021-02-23 23:31:51 +0300

    firs submit

---
+++ Badger/src/Firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import * as firebase from 'firebase';
+
+//Firebase:ImageDetection
+//VisionApi:ImageDetectionVisionApi
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBs6f4TvbsKAjxTHPVf9bKx6ql43u3HSl8',
+  authDomain: 'imagedetection-3756c.firebaseapp.com',
+  projectId: 'imagedetection-3756c',
+  storageBucket: 'imagedetection-3756c.appspot.com',
+  messagingSenderId: '481087796115',
+  appId: '1:481087796115:web:b1471adc12210617ecae66',
+});
+
+
+export default firebase;
